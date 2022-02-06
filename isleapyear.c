d127f1a37f9e0d810954266e3a1726dde4fbbdad
lau1944 <a954723219@gmail.com>
2022-02-06 13:46:43 +0800

    fix: bug fixed

--- isleapyear.c
+++ isleapyear.c
@@ -5,11 +5,12 @@
 int isLeapYear(int year);
 
 int main(int argc, char *argv[]) {
-    int year;
+    char year[4];
     char output_str[30];
-    scanf("%d", &year);  
-    int isLeap = isLeapYear(year);
-    sprintf(output_str, isLeap == 0 ? "%d was not a leap year" : "%d was a leap year", year);
+    scanf("%s", year);  
+    int year_in_num = atoi(year);
+    int isLeap = isLeapYear(year_in_num);
+    sprintf(output_str, isLeap == 0 ? "%d was not a leap year" : "%d was a leap year", year_in_num);
     printf("%s", output_str);
 }
 
