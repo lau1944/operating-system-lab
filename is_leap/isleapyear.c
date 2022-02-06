93ab00b15a9c54915308168c8022fbd1ac570b0f
lau1944 <a954723219@gmail.com>
2022-02-06 18:42:36 +0800

    fix: fix code in order to pass auto grade

--- is_leap/isleapyear.c
+++ is_leap/isleapyear.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int isLeapYear(int year);
 
 int main(int argc, char *argv[]) {
-    int year;
-    printf("Enter an year: ");
-    scanf("%d", &year);  
-    int isLeap = isLeapYear(year);
-    printf(isLeap == 0 ? "%d was not a leap year" : "%d was a leap year", year);
+    char year[30];
+    char output_str[30];
+    scanf("%s", year);  
+    int year_in_num = atoi(year);
+    int isLeap = isLeapYear(year_in_num);
+    sprintf(output_str, isLeap == 0 ? "%d was not a leap year" : "%d was a leap year", year_in_num);
+    printf("%s", output_str);
 }
 
 int isLeapYear(int year) {
