ae0e294fa86794666338716de874acfa55c28e90
lau1944 <a954723219@gmail.com>
2022-02-20 20:18:07 +0800

    bug fix

--- cpu.c
+++ cpu.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 struct PCB NULLPCB = {0, 0, 0, 0, 0, 0, 0};
 
-void remove_pcb(struct PCB *array[], int index, int array_length);
+void remove_pcb(struct PCB array[], int index, int array_length);
 
 int is_empty_queue(struct PCB queue[QUEUEMAX]);
 
@@ -80,7 +80,7 @@ struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *q
         }
     }
     // remove from ready queue
-    remove_pcb(&ready_queue, max_process_index, *queue_cnt);
+    remove_pcb(ready_queue, max_process_index, *queue_cnt);
     --(*queue_cnt);
     max_priority_process.execution_starttime = timestamp;
     max_priority_process.execution_endtime = timestamp + max_priority_process.remaining_bursttime;
@@ -141,7 +141,7 @@ struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int 
         }
     }
     // remove from ready queue
-    remove_pcb(&ready_queue, short_remain_time_process_index, *queue_cnt);
+    remove_pcb(ready_queue, short_remain_time_process_index, *queue_cnt);
     --(*queue_cnt);
     short_remain_time_process.execution_starttime = timestamp;
     short_remain_time_process.execution_endtime = timestamp + short_remain_time_process.remaining_bursttime;
@@ -187,7 +187,7 @@ struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *q
         }
     }
     // remove from ready queue
-    remove_pcb(&ready_queue, early_arrive_time_process_index, *queue_cnt);
+    remove_pcb(ready_queue, early_arrive_time_process_index, *queue_cnt);
     --(*queue_cnt);
     early_arrive_time_process.execution_starttime = timestamp;
     early_arrive_time_process.execution_endtime = timestamp + early_arrive_time_process.remaining_bursttime;
@@ -212,7 +212,7 @@ int is_null_pcb(struct PCB process)
 }
 
 // remove pcb from ready queue
-void remove_pcb(struct PCB *array[], int index, int array_length)
+void remove_pcb(struct PCB array[], int index, int array_length)
 {
     for (int i = index; i < array_length - 1; ++i)
     {
