a15edc4f5127e1307183020e43bc4bc58c9168be
lau1944 <a954723219@gmail.com>
2022-03-13 17:04:40 +0800

    bug fixed

--- virtual.c
+++ virtual.c
@@ -3,22 +3,20 @@
 #include <stdio.h>
 #define DUMB_TIMESTAMP -100
 
-// int main() {
+// int main()
+// {
 //     struct PTE p1 = {
-//         -1, -1, -1, -1, -1
-//     };
+//         -1, -1, -1, -1, -1};
 //     struct PTE p2 = {
-//         1, 10, 3, 3, 1
-//     };
+//         1, 10, 3, 3, 1};
 //     struct PTE p3 = {
-//         1, 30, 1, 1, 1
-//     };
+//         1, 30, 1, 1, 1};
 //     int table_cnt = 3;
 //     int frame_cnt = 0;
 //     struct PTE ptes[TABLEMAX] = {p1, p2, p3};
 //     int frame_pool[POOLMAX];
-//     int frame = process_page_access_fifo(ptes, &table_cnt, 0, frame_pool, &frame_cnt, 30);
-//     printf("%d", ptes[2].frame_number);
+//     int frame = process_page_access_lfu(ptes, &table_cnt, 0, frame_pool, &frame_cnt, 30);
+//     printf("%d", frame);
 // }
 
 int poll(int frame_pool[POOLMAX], int *frame_cnt);
@@ -151,7 +149,7 @@ int process_page_access_lru(struct PTE page_table[TABLEMAX], int *table_cnt, int
         page_table[page_number].frame_number = frame_number;
         page_table[page_number].last_access_timestamp = current_timestamp;
         page_table[page_number].arrival_timestamp = current_timestamp;
-        page_table[page_number].reference_count += 1;
+        page_table[page_number].reference_count = 1;
         return frame_number;
     }
     else
@@ -262,18 +260,23 @@ int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int
         page_table[page_number].frame_number = frame_number;
         page_table[page_number].last_access_timestamp = current_timestamp;
         page_table[page_number].arrival_timestamp = current_timestamp;
-        page_table[page_number].reference_count += 1;
+        page_table[page_number].reference_count = 1;
         return frame_number;
     }
     else
     {
         int smallest_ref_count = -1;
-        int target_page_index;
+        int target_page_index = -1;
         for (int i = 0; i < *table_cnt; ++i)
         {
             if (page_table[i].is_valid == 1)
             {
-                if (smallest_ref_count < page_table[i].reference_count || smallest_ref_count == page_table[i].reference_count && page_table[target_page_index].arrival_timestamp <= page_table[i].arrival_timestamp)
+                if (target_page_index == -1)
+                {
+                    target_page_index = i;
+                    continue;
+                }
+                if (smallest_ref_count < page_table[i].reference_count || (smallest_ref_count == page_table[i].reference_count && page_table[target_page_index].arrival_timestamp <= page_table[i].arrival_timestamp))
                     continue;
 
                 smallest_ref_count = page_table[i].reference_count;
